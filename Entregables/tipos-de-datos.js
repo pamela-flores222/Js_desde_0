d3d3ffeb4c17e34bd28a974326254d388e5db061
pamela-flores222 <pame_capri27@hotmail.com>
2026-02-28 10:22:37 -0600

    ejemplos de tipos de datos

---
+++ Entregables/tipos-de-datos.js
@@ -0,0 +1,47 @@
+//Tipos de datos en javascript
+
+//typeof devuelve el tipo de dato que es la variable
+
+console.log("Diferentes tipos de datos");
+
+let edad = 42
+console.log(edad + " es tipo de dato:")
+console.log(typeof edad );
+
+let palabra = "veinticinco"
+console.log(palabra + " es tipo de dato:")
+console.log(typeof palabra);
+
+let negativo = -666
+console.log(negativo + " es tipo de dato:")
+console.log(typeof negativo);
+
+let afirmacion = true
+console.log(afirmacion + " es tipo de dato:")
+console.log(typeof afirmacion);
+
+let cero = 0
+console.log(cero + " es tipo de dato:")
+console.log(typeof cero);
+
+let comillas = ""
+console.log(comillas + " es tipo de dato:")
+console.log(typeof comillas);
+
+let nulo = null
+console.log(nulo + " es tipo de dato:")
+console.log(typeof nulo); //sale object porque es un error historico no corregido
+
+let vacio = undefined
+console.log(vacio + " es tipo de dato:")
+console.log(typeof undefined);
+
+let falso = false
+console.log(falso + " es tipo de dato:")
+console.log(typeof falso);
+
+
+
+
+
+
